6bdf51ddb0075269150f316575f0db561c77bbc7
sousajo <josesousa@rentalcars.com>
2020-08-21 00:23:52 +0100

    Using @Primary annotation

--- src/main/java/com/springstudy/sfdistudy/services/PrimaryGreetingService.java
+++ src/main/java/com/springstudy/sfdistudy/services/PrimaryGreetingService.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Service;
  * Created by sousaJ on 21/08/2020
  * in package - com.springstudy.sfdistudy.services
  **/
+
+// A Bean defined as Primary in this case will be set as "default"
+// the controller MyController is injecting the GreetingService Interface but
+// doesn't have the @Qualifier annotation defining which service should be injected
+// so the Bean that will be injected is the one defined as default, set as @Primary
+
 @Primary
 @Service
 public class PrimaryGreetingService implements GreetingService{
