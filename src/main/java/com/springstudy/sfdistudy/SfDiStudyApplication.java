9e2aa56ad9d530a19c525194bb8b898440dd35c8
sousajo <josesousa@rentalcars.com>
2020-08-24 11:56:41 +0100

    Using default profile in spring

--- src/main/java/com/springstudy/sfdistudy/SfDiStudyApplication.java
+++ src/main/java/com/springstudy/sfdistudy/SfDiStudyApplication.java
@@ -1,9 +1,6 @@
 package com.springstudy.sfdistudy;
 
-import com.springstudy.sfdistudy.controllers.ConstructorInjectedController;
-import com.springstudy.sfdistudy.controllers.MyController;
-import com.springstudy.sfdistudy.controllers.PropertyInjectedController;
-import com.springstudy.sfdistudy.controllers.SetterInjectedController;
+import com.springstudy.sfdistudy.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +12,10 @@ public class SfDiStudyApplication {
         ApplicationContext ctx = SpringApplication.run(SfDiStudyApplication.class, args);
 
         MyController myController = (MyController)ctx.getBean("myController");
+
+        System.out.println("---------- Profile study");
+        I18NController i18NController = (I18NController) ctx.getBean("i18NController");
+        System.out.println("i18NController.sayHello() = " + i18NController.sayHello());
 
         System.out.println("---------- Primary Bean");
         System.out.println("greeting Primary Bean = " + myController.sayHello());
---
+++ src/main/java/com/springstudy/sfdistudy/services/I18nSpanishGreetingService.java
@@ -0,0 +1,17 @@
+package com.springstudy.sfdistudy.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sousaJ on 24/08/2020
+ * in package - com.springstudy.sfdistudy.services
+ **/
+@Profile("ES")
+@Service("i18nService")
+public class I18nSpanishGreetingService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Holla Mundo";
+    }
+}
