4806b0253c7adb617ff8906cae61e50936873703
sousajo <josesousa@rentalcars.com>
2020-08-24 12:02:28 +0100

    Using default profile in spring

--- src/main/java/com/springstudy/sfdistudy/services/I18nSpanishGreetingService.java
+++ src/main/java/com/springstudy/sfdistudy/services/I18nSpanishGreetingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
  * Created by sousaJ on 24/08/2020
  * in package - com.springstudy.sfdistudy.services
  **/
-@Profile("ES")
+@Profile({"ES", "default"}) // setting this profile as default
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
